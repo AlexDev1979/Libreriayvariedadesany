019aef03737108d0e3caeca6ec9487e128daad87
AlexDev1979 <wilgar270@hotmail.com>
2025-03-17 23:00:27 -0600

    Arquitectura de proyecto

---
+++ Libreriayvariedadesany/Libreriayvariedadesany.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Libreriayvariedadesany.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
