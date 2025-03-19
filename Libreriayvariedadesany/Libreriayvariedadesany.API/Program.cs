76593d10feaf00916de1520ca1cd24984e550a0f
AlexDev1979 <wilgar270@hotmail.com>
2025-03-18 19:02:14 -0600

    Begin countries API controller

--- Libreriayvariedadesany/Libreriayvariedadesany.API/Program.cs
+++ Libreriayvariedadesany/Libreriayvariedadesany.API/Program.cs
@@ -1,11 +1,14 @@
+using Libreriayvariedadesany.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(x =>x.UseSqlServer("name=LocalConnection"));
 
 var app = builder.Build();
 
@@ -17,9 +20,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+app.UseCors(x => x
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .SetIsOriginAllowed(origin => true)
+        .AllowCredentials());
+
 
 app.Run();
---
+++ Libreriayvariedadesany/Libreriayvariedadesany.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Libreriayvariedadesany.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} carácter.")]
+        public string Name { get; set; } = null!;
+    }
+}
